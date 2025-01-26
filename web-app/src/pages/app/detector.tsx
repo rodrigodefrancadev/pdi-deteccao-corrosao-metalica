878428016c11b33d15afed2211c22fcad4e21cab
Rodrigo de França <rodrigodefrancadev@gmail.com>
2025-01-26 09:39:03 -0300

    cavas desenhando camera ok

--- web-app/src/pages/app/detector.tsx
+++ web-app/src/pages/app/detector.tsx
@@ -1,21 +1,22 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Detector: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [erro, setErro] = useState(false);
 
   useEffect(() => {
     let stream: MediaStream | undefined;
     const getVideo = async () => {
       try {
         stream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: { ideal: "environment" } }, // Use rear camera on mobile devices
+          video: { facingMode: { ideal: "environment" }, aspectRatio: 1 },
         });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
-      } catch (err) {
-        console.error("Error accessing the camera: ", err);
+      } catch {
+        setErro(true);
       }
     };
 
@@ -29,6 +30,8 @@ const Detector: React.FC = () => {
   }, []);
 
   useEffect(() => {
+    let requestAnimationFrameId: number | undefined;
+
     const drawVideoOnCanvas = () => {
       if (videoRef.current && canvasRef.current) {
         const context = canvasRef.current.getContext("2d");
@@ -40,18 +43,57 @@ const Detector: React.FC = () => {
             canvasRef.current.width,
             canvasRef.current.height
           );
+          const squareSize = canvasRef.current.width * 0.5;
+          const x = (canvasRef.current.width - squareSize) / 2;
+          const y = (canvasRef.current.height - squareSize) / 2;
+          context.strokeStyle = "red";
+          context.lineWidth = 5;
+          context.strokeRect(x, y, squareSize, squareSize);
         }
       }
-      requestAnimationFrame(drawVideoOnCanvas);
+      requestAnimationFrameId = requestAnimationFrame(drawVideoOnCanvas);
     };
 
     drawVideoOnCanvas();
+
+    return () => {
+      if (requestAnimationFrameId !== undefined) {
+        cancelAnimationFrame(requestAnimationFrameId);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const resizeCanvas = () => {
+      if (canvasRef.current) {
+        const _size = Math.min(window.innerWidth, window.innerHeight);
+        const size = Math.min(_size, 700);
+        canvasRef.current.width = size;
+        canvasRef.current.height = size;
+      }
+    };
+
+    window.addEventListener("resize", resizeCanvas);
+    resizeCanvas();
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+    };
   }, []);
 
   return (
     <div className="detector-container">
-      <video ref={videoRef} autoPlay style={{ display: "none" }} />
-      <canvas ref={canvasRef} style={{ width: "100%" }} />
+      {erro ? (
+        <p className="error-text" style={{ textAlign: "center" }}>
+          Permissão Negada! Para autorizar o acesso á câmera acesse as
+          configurações do seu navegador.
+        </p>
+      ) : (
+        <>
+          <video ref={videoRef} autoPlay style={{ display: "none" }} />
+          <canvas ref={canvasRef} />
+        </>
+      )}
     </div>
   );
 };
